bda44490ae00459a5eb4c4d26e7eece2b1ae4c8a
Lê Văn Trung <69235532+sevenghost-2001@users.noreply.github.com>
2023-12-01 15:15:25 +0700

    API

---
+++ ValiStore/Controllers/ProductAPIController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ValiStore.Models;
+using ValiStore.Models.ProductModels;
+
+namespace ValiStore.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductAPIController : ControllerBase
+    {
+        QLBanVaLiContext db = new QLBanVaLiContext();
+        [HttpGet]
+        public IEnumerable<Product> GetAllProducts()
+        {
+            var sanPham = (from p in db.TDanhMucSps
+                           select new Product
+                           {
+                               MaSp = p.MaSp,
+                               TenSp = p.TenSp,
+                               MaLoai = p.MaLoai,
+                               AnhDaiDien = p.AnhDaiDien,
+                               GiaNhoNhat = p.GiaNhoNhat
+                           }).ToList();
+            return sanPham;
+        }
+        [HttpGet("{maloai}")]
+        public IEnumerable<Product> GetProductsByCategory(string maloai)
+        {
+            var sanPham = (from p in db.TDanhMucSps
+                           where p.MaLoai== maloai
+                           select new Product
+                           {
+                               MaSp = p.MaSp,
+                               TenSp = p.TenSp,
+                               MaLoai = p.MaLoai,
+                               AnhDaiDien = p.AnhDaiDien,
+                               GiaNhoNhat = p.GiaNhoNhat
+                           }).ToList();
+            return sanPham;
+        }
+    }
+}
